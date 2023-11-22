7f3f30fcd56c35b210b2b2348274deabd4d98990
Remastered <99067800@mydavinci.nl>
2023-11-22 01:27:44 -1100

    Added 2FA test

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-
+  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
+  
 Route::get('/', function () {
     return view('welcome');
 });
+  
+Auth::routes();
+  
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('2fa');
+  
+Route::get('2fa', [App\Http\Controllers\TwoFAController::class, 'index'])->name('2fa.index');
+Route::post('2fa', [App\Http\Controllers\TwoFAController::class, 'store'])->name('2fa.post');
+Route::get('2fa/reset', [App\Http\Controllers\TwoFAController::class, 'resend'])->name('2fa.resend');
