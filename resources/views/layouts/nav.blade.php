9422458ca0efb0bc6b56c4f703781d1499c41fad
LaraMol <89733492+LaraMol@users.noreply.github.com>
2023-12-13 18:02:25 +0100

    Fix navbar name changes
    This is the CSS fix for the navbar change

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,21 +7,23 @@
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <a class="nav-link active color" aria-current="page" href="{{route('welcome')}}">Home</a>
+                    <a class="nav-link active color" aria-current="page" href="{{route('home')}}">Home</a>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{route('dashboard')}}">Dashboard</a>
+                    {{-- <a class="nav-link active color" aria-current="page" href="{{route('device.index')}}">Device</a> --}}
                 </li>
                 </li>
                 <li class="nav-item">
-
+                    <a class="nav-link" aria-current="page" href="{{route('starters')}}">Main page</a>
                 </li>
             </ul>
         </div>
     </div>
     <div class="dropdown">
         <button onclick="myFunction()" class="dropbtn">Menu</button>
-            <a href="{{Route("device.index")}}">Configuration Menu</a>
-            <a href="../settings/settings.php">Settings</a>
+        <div id="myDropdown" class="dropdown-content" style="z-index: 100;">
+            <a href="{{route('device.index')}}">Configuration Menu</a>
+            <a href="../settings/settings.php">User Settings</a>
+            {{-- <a href="{{Route("device.index")}}">devices</a> --}}
             <a href="#Logout">Logout</a>
         </div>
     </div>
