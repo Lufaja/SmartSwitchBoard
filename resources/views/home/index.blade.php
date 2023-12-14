2476af13d26ab0258aee90ae486b0a375bb48f7e
Jeroen-dH <proskater7437@gmail.com>
2023-12-14 22:35:29 +0100

    Basic styling done

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,7 +14,7 @@
 <div class="home">
     <div class="parent">
         <div class="div1">
-            <div class="profile background">
+            <div class="profile background item">
                 <div class="ship-name">
                     <h1>Scopus</h1>
                 </div>
@@ -25,34 +25,49 @@
         </div>
 
         <div class="div2">
-            <div class="information background">
+            <div class="background item">
                 <div>
                     <h2 class="fw-bold">Van der leun</h2>
                 </div>
-                <div class="contact">
-                    Nummer
-                    Website
-                    instagram
-                    facebook
+                <div class="Text">
+                    <div>Nummer</div>
+                    <div>Website</div>
+                    <div>instagram</div>
+                    <div>facebook</div>
                 </div>
             </div>    
         </div>
 
         <div class="div3">
-            <div class="ship-info background">
+            <div class="ship-info background item">
                 <h2 class="fw-bold">My ship</h2>
+                <div class="Text">
+                    <div>Ship info</div>
+                    <div>Ship info</div>
+                    <div>Ship info</div>
+                    <div>Ship info</div>
+                </div>
             </div>        
         </div>
 
         <div class="div4">
-            <div class="ship-info2 background">
-                .
+            <div class="ship-info2 background item">
+                <h2 class="fw-bold">info</h2>
+                <div class="Text">
+                    <div>info</div>
+                    <div>info</div>
+                    <div>info</div>
+                    <div>info</div>
+                </div>
             </div>
         </div>
 
         <div class="div5">
-            <div class="log background">
-                .
+            <div class="background item">
+                <h2 class="fw-bold">Logs</h2>
+                <div class="logs">
+                    
+                </div>
             </div>
         </div>
     </div> 
