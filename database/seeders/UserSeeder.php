b5832ff9f29f6ff0737e3ef8a5054485568d41eb
Lukas van Hulst <Lukas.v.hulst@gmail.com>
2023-12-15 10:02:15 +0100

    added user seeder for the database with test account

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '+31639888442',
+            'password' => Hash::make('admin')
+        ]);
+    }
+}
